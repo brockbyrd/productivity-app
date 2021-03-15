e0c9cdaa9f78c3f72da245ba3afac05470b21c0e
brockbyrd <brockbyrdd@gmail.com>
2021-03-15 12:26:05 -0500

    creates the form to add a todo

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function TodoInput() {
+export default function TodoInput({ addTodo }) {
+    const [todo, setTodo] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addTodo([todo])
+        setTodo('')
+    }
     return (
-        <div className="todo__input">
-            <label for="Todo">Todo: </label>
-            <input type="text" name="todo" placeholder="Todo"/>
-            <input type="submit" />
-        </div>
+        <form className="todo__form" onSubmit={handleSubmit}>
+            <label for="todo">Todo: </label>
+            <input type="text" name="todo" value={todo} placeholder="Todo" onChange={e => setTodo(e.target.value)} />
+            <input type="submit" value="Add Todo" />
+        </form>
     )
 }
