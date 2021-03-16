8b169738040c01ca5b8a37b1c08dcb8aa1377ad8
brockbyrd <brockbyrdd@gmail.com>
2021-03-16 14:50:52 -0500

    imports reward list and displays it to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import TodoList from './containers/TodoList'
 import GoalsList from './containers/GoalsList'
+import RewardsList from './containers/RewardsList'
 
 function App() {
   return (
     <div className="App">
       <GoalsList />
       <TodoList />
+      <RewardsList />
     </div>
   );
 }
