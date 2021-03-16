db4f87392f48969dd458e8917a6764cdfe7b4e01
brockbyrd <brockbyrdd@gmail.com>
2021-03-16 10:43:05 -0500

    imports reward item and input to list

--- src/containers/RewardsList.js
+++ src/containers/RewardsList.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
+import RewardItem from '../components/RewardItem'
+import RewardInput from '../components/RewardInput'
 
 export default function RewardsList() {
     const [rewards, setRewards] = useState([])
-    return (
-        <div>
 
+    return (
+        <div className="rewards__list">
+            <RewardInput />
+            <RewardItem />
         </div>
     )
 }
