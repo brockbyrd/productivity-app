d9e5deb78484ee1f9231323e955254d0f9d8882e
brockbyrd <brockbyrdd@gmail.com>
2021-03-17 12:17:15 -0500

    imports list from semantic UI to reward item

--- src/components/RewardItem.js
+++ src/components/RewardItem.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import { List } from 'semantic-ui-react'
 
 export default function RewardItem(props) {
     const rewards = props.rewards
 
     return (
-        <ul>
+        <List>
             {rewards.map((reward) =>
                 <>
-                <li>{reward}</li>
+                <List.Item>{reward}</List.Item>
                 </>
             )}
-        </ul>
+        </List>
     )
 }
