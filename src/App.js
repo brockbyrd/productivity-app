19211b704d64d3bed85c86b96e7d18e3ec1ae405
brockbyrd <brockbyrdd@gmail.com>
2021-03-22 15:59:27 -0500

    imports semantic ui and styles app into grid

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
+import { Grid } from 'semantic-ui-react';
 import TodoContainer from './containers/TodoContainer'
 import GoalsContainer from './containers/GoalsContainer'
 import RewardsContainer from './containers/RewardsContainer'
 
 function App() {
   return (
-    <div className="App">
-      <GoalsContainer />
-      <TodoContainer />
-      <RewardsContainer />
+    <div className="app">
+      <Grid columns={3} divided>
+        <Grid.Row>
+          <Grid.Column>
+            <GoalsContainer />
+          </Grid.Column>
+          <Grid.Column>
+            <TodoContainer />
+          </Grid.Column>
+          <Grid.Column>
+            <RewardsContainer />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
   );
 }
