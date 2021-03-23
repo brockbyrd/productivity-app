73239b448f9311c2d369acee10f3f0ead63ea14e
brockbyrd <brockbyrdd@gmail.com>
2021-03-23 13:53:49 -0500

    puts todos and goals into grid container

--- src/containers/GoalsContainer.js
+++ src/containers/GoalsContainer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import useStickyState from '../customHooks/useStickyState'
 import GoalInput from '../components/GoalInput'
 import GoalList from '../components/GoalList'
+import { Grid } from 'semantic-ui-react';
 
 export default function GoalContainer() {
     const [goals, setGoals ] = useStickyState([], 'goals')
@@ -16,10 +17,26 @@ export default function GoalContainer() {
     }
 
     return (
-        <div className="goals__list">
-            <h1>Weekly Goals</h1>
-            <GoalInput addGoal={addGoal} />
-            <GoalList goals={goals} onRemove={handleRemove} />
+        <div className="goals__container">
+            <Grid centered columns={2}>
+                <Grid.Row>
+                    <Grid.Column>
+                        <h1>Weekly Goals</h1>
+                    </Grid.Column>
+                </Grid.Row>
+
+                <Grid.Row centered columns={1}>
+                    <Grid.Column>
+                        <GoalInput addGoal={addGoal} />
+                    </Grid.Column>
+                </Grid.Row>
+
+                <Grid.Row centered columns={2}>
+                    <Grid.Column>
+                        <GoalList goals={goals} onRemove={handleRemove} />
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
         </div>
     )
 }
--- src/containers/TodoContainer.js
+++ src/containers/TodoContainer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import useStickyState from '../customHooks/useStickyState';
 import TodoInput from '../components/TodoInput'
 import TodoList from '../components/TodoList'
+import { Grid } from 'semantic-ui-react';
 
 export default function TodoContainer() {
     const [todos, setTodos] = useStickyState([], 'todos')
@@ -17,9 +18,26 @@ export default function TodoContainer() {
 
     return (
         <div className="todos__list">
-            <h1>Daily Todos</h1>
-            <TodoInput addTodo={addTodo} />
-            <TodoList todos={todos} onRemove={handleRemove} />
+            <Grid centered columns={2}>
+                <Grid.Row>
+                    <Grid.Column>
+                        <h1>Daily Todos</h1>
+                    </Grid.Column>
+                </Grid.Row>
+
+                <Grid.Row centered columns={1}>
+                    <Grid.Column>
+                        <TodoInput addTodo={addTodo} />
+                    </Grid.Column>
+                </Grid.Row>
+
+                <Grid.Row centered columns={2}>
+                    <Grid.Column>
+                        <TodoList todos={todos} onRemove={handleRemove} />
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+
         </div>
     )
 }
