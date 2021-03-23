61cfe1dbb65397cf51a4a9712cfb51877e067675
brockbyrd <brockbyrdd@gmail.com>
2021-03-23 15:59:55 -0500

    puts app into a container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Container } from 'semantic-ui-react';
 import TodoContainer from './containers/TodoContainer'
 import GoalsContainer from './containers/GoalsContainer'
 import RewardsContainer from './containers/RewardsContainer'
@@ -7,19 +7,24 @@ import RewardsContainer from './containers/RewardsContainer'
 function App() {
   return (
     <div className="app">
-      <Grid columns={3} divided>
-        <Grid.Row>
-          <Grid.Column>
-            <GoalsContainer />
-          </Grid.Column>
-          <Grid.Column>
-            <TodoContainer />
-          </Grid.Column>
-          <Grid.Column>
-            <RewardsContainer />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+      <Container fluid>
+        <Grid columns={3} divided>
+          <Grid.Row>
+            Decoration
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <GoalsContainer />
+            </Grid.Column>
+            <Grid.Column>
+              <TodoContainer />
+            </Grid.Column>
+            <Grid.Column>
+              <RewardsContainer />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
     </div>
   );
 }
