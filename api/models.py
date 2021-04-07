8f95eff140e877ac4b29478a67cb7a68276c1abd
brockbyrd <brockbyrdd@gmail.com>
2021-04-07 15:24:19 -0500

    creates rewards and goal model

--- api/models.py
+++ api/models.py
@@ -4,3 +4,13 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String)
     points = db.Column(db.Integer)
+
+class Goal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String)
+    points = db.Column(db.Integer)
+
+class Reward(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String)
+    points = db.Column(db.Integer)
