fef0384d938a27cef0f3498e01d524774aea9bf9
brockbyrd <brockbyrdd@gmail.com>
2021-04-07 13:18:47 -0500

    creates add_todo function and queries todos to be shown as json to frontend

--- api/views.py
+++ api/views.py
@@ -1,15 +1,26 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from . import db
+from .models import Todo
 
 main = Blueprint('main', __name__)
 
 @main.route('/add_todo', methods=['POST'])
 def add_todo():
+    todo_data = request.get_json()
+
+    new_todo = Todo(content=todo_data['content'], points=todo_data['points'])
+
+    db.session.add(new_todo)
+    db.session.commit()
 
     return 'Done', 201
 
 @main.route('/todos')
 def todos():
-
+    todo_list = Todo.query.all()
     todos = []
+
+    for todo in todo_list:
+        todos.append({'content' : todo.content, 'points' : todo.points})
 
     return jsonify({'todos' : todos})
